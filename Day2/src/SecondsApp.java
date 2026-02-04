9c1befb030f7ba0b6f815ad999ebf05f073e25e4
Alex-935 <alex.potter3@hotmail.co.uk>
2026-02-04 07:23:28 +0000

    Day2

--- Day2/src/SecondsApp.java
+++ Day2/src/SecondsApp.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class SecondsApp {
 
     public static void main() {
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Please enter the number of seconds: ");
+        int totalSeconds = scanner.nextInt();
+
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+
+        int hours = minutes / 60;
+        minutes %= 60;
+
+        System.out.printf("""
+                Input      Hours      Minutes      Seconds
+                %-6d    %6d       %6d       %6d
+                """, totalSeconds, hours, minutes, seconds);
     }
 }
