2dbfa6370c2810995d64a2ce5d005594ea865d89
Alex-935 <alex.potter3@hotmail.co.uk>
2026-02-04 18:32:00 +0000

    Day3

--- Day3/src/AverageApp.java
+++ Day3/src/AverageApp.java
@@ -1,4 +1,7 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+
 
 public class AverageApp {
 
@@ -6,22 +9,13 @@ public class AverageApp {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Please enter the first integer: ");
-        int num1 = scanner.nextInt();
-        scanner.nextLine();
+        //get four integers
+        int num1 = CalculatorApp.getInt("first");
+        int num2  = CalculatorApp.getInt("second");
+        int num3 = CalculatorApp.getInt("third");
+        int num4 = CalculatorApp.getInt("fourth");
 
-        System.out.print("Please enter the second integer: ");
-        int num2  = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("Please enter the third integer: ");
-        int num3  = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("Please enter the fourth integer: ");
-        int num4  = scanner.nextInt();
-        scanner.nextLine();
-
+        // by making one number a float, all the others are converted into a float
         System.out.println("The result is: " + ((float) num1 + num2 + num3 + num4) / 4);
     }
 
--- Day3/src/CalculatorApp.java
+++ Day3/src/CalculatorApp.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalculatorApp {
@@ -6,14 +7,65 @@ public class CalculatorApp {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Please enter the first integer: ");
-        int num1 = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("Please enter the second integer: ");
-        int num2  = scanner.nextInt();
-        scanner.nextLine();
+        int num1 = getInt("first");
+        int num2 = getInt("second");
 
         System.out.println("The result is: " + ((float) num1 / num2));
+    }
+
+
+    //Asks the user for an integer input and validates it
+    public static int getInt(String position) {
+
+        // create scanner
+        Scanner scanner = new Scanner(System.in);
+        // create variable to
+        boolean validated = false;
+
+        do {
+            System.out.printf("Please enter the %s integer: ", position);
+            //attempt to convert string to int
+            try {
+                int num = Integer.parseInt(scanner.nextLine());
+                //scanner.nextLine();
+
+                return num;
+            }
+            // if the user didn't input an integer
+            catch (NumberFormatException e) {
+                System.out.println("Please make sure you enter an integer");
+            }
+
+        } while (!validated);
+
+        return -1;
+    }
+
+    //Asks the user for an integer input and validates it
+    // version for no argument
+    static int getInt() {
+
+        // create scanner
+        Scanner scanner = new Scanner(System.in);
+        // create variable to
+        boolean validated = false;
+
+        do {
+            System.out.printf("Please enter the %s integer: ");
+            //attempt to convert string to int
+            try {
+                int num = Integer.parseInt(scanner.nextLine());
+                //scanner.nextLine();
+
+                return num;
+            }
+            // if the user didn't input an integer
+            catch (InputMismatchException e) {
+                System.out.println("Please make sure you enter an integer");
+            }
+
+        } while (!validated);
+
+        return -1;
     }
 }
